225a21ee2b5eedc447d3d2d9d7ead223b096e364
Lucas Oliveira <lfo@icomp.ufam.edu.br>
2019-11-08 13:38:29 -0400

    Problemas com java.time.Instant

--- src/main/java/br/com/estacionamento/EstacionamentoApplication.java
+++ src/main/java/br/com/estacionamento/EstacionamentoApplication.java
@@ -1,16 +1,29 @@
 package br.com.estacionamento;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableAsync
 public class EstacionamentoApplication {
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(EstacionamentoApplication.class, args);
     }
 
+    @PostConstruct
+    public void setUp() {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.findAndRegisterModules();
+    }
 }
